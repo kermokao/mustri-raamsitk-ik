f48c8eec7040bd0b9d4aa7511209820726ea0e8c
kermo kao <kermo.kao@gmail.com>
2026-09-17 10:28:41 +0300

    delete tasks data from files

--- index.js
+++ index.js
@@ -61,8 +61,29 @@ app.post("/", (req, res) =>{
                 res.redirect("/")
             }) 
         }) 
-}) 
+})
+
+app.get("/delete-task/:taskId", (req, res) => {
+    let deletedTaskId = Number(req.params.taskId)
+    readFile("./tasks.json")
+    .then(tasks => {
+        tasks.forEach((task, index) => {
+            if(task.id == deletedTaskId) {
+                tasks.splice(index, 1)
+            }               
+    })
+    const data = JSON.stringify(tasks, null, 2)
+    fs.writeFile("./tasks.json", data, "utf-8", err => {
+        if (err) {
+            return;
+        }
+        res.redirect("/") 
+    })
+   }) 
+})
+
+
  
 app.listen(3001, () => {
     console.log("Example app is started at http://localhost:3001")
- })   
+})   
